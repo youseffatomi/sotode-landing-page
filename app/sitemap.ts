586756ebabc5edcd9cf5c7494186cca477c1c87f
joseph <joseph00631@gmail.com>
2026-06-08 14:54:05 +0330

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,16 @@
-// app/sitemap.ts
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.hossein-sotoudeh.ir",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "weekly",
       priority: 1,
+      images: [
+        "https://www.hossein-sotoudeh.ir/_next/image?url=%2Fimages%2Fprofile.png&w=1920&q=75",
+        "https://www.hossein-sotoudeh.ir/_next/image?url=%2Fimages%2F%D8%AD%D8%B3%DB%8C%D9%86_%D8%B3%D8%AA%D9%88%D8%AF%D9%87.jpg&w=1920&q=75",
+      ],
     },
   ];
 }
