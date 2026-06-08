2548d5632c4750205dbca913e609c686e720cb84
joseph <joseph00631@gmail.com>
2026-06-08 14:52:53 +0330

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,13 @@
-import type { MetadataRoute } from "next";
+// app/sitemap.ts
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://acme.com",
+      url: "https://www.hossein-sotoudeh.ir",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "monthly",
       priority: 1,
-      images: [
-        "https://www.hossein-sotoudeh.ir/_next/image?url=%2Fimages%2Fprofile.png&w=1920&q=75",
-        "https://www.hossein-sotoudeh.ir/_next/image?url=%2Fimages%2F%D8%AD%D8%B3%DB%8C%D9%86_%D8%B3%D8%AA%D9%88%D8%AF%D9%87.jpg&w=1920&q=75",
-      ],
     },
   ];
 }
